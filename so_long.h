de29b4f843a281547bf153f50887a39f7e8da9e0
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-02-02 22:23:58 +0100

    impliment checkwall()

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 10:38:33 by mel-kouc          #+#    #+#             */
-/*   Updated: 2023/01/31 18:39:49 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2023/02/02 10:53:31 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 # define SO_LONG_H
 
 # include <fcntl.h>
-# include "./Get-Next-Line/get_next_line.h"
-# include "./Libft/libft.h"
+# include "libft/libft.h"
+# include "Get-Next-Line/get_next_line.h"
+
 // char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
 
 #endif
--- test.c
+++ test.c
@@ -6,29 +6,63 @@
 /*   By: mel-kouc <mel-kouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:50:48 by mel-kouc          #+#    #+#             */
-/*   Updated: 2023/01/31 21:39:58 by mel-kouc         ###   ########.fr       */
+/*   Updated: 2023/02/02 22:22:48 by mel-kouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
-// read map
-int	main(void)
-{
-	char	*linemap;
-	char	*sjoin;
-	int		fd;
+// void	checkwall(char **line, int count, int len)
+// {
+// 	int	i;
+// 	int	j;
 
-	fd = open("map.ber", O_RDONLY);
-	linemap = get_next_line(fd);
+// 	i = 1;
+// 	while (i <= count)
+// 	{
+// 		j = 0;
+// 		while (j < len)
+// 		{
+// 			if (i == 0 || i == count)
+// 		}
+// 	}
+
+// }
+
+// read map , push it in array 2 dimention
+void	readmap(int fd, char *linemap)
+{
+	static char	*sjoin;
+	char		**divide;
+	int			count;
+	int			len;
+
+	count = 0;
+	len = ft_strlen(linemap);
+	printf("%d", len);
 	if (!sjoin)
-		ft_strdup("");
+		sjoin = ft_strdup("");
 	while (linemap)
 	{
-		printf("%s", linemap);
-		sjoin = ft_strjoin(sjoin, linemap);
+		sjoin = ft_strxjoin(sjoin, linemap);
+		count++;
 		linemap = get_next_line(fd);
 	}
-	printf("%s", sjoin);
+	// printf("%d", count);
+	divide = ft_split(sjoin, '\n');
+	checkwall(divide, count, len);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*linemap;
+
+	fd = open("map.ber", O_RDONLY);
+	if (fd == -1)
+		exit(1);
+	linemap = get_next_line(fd);
+	readmap(fd, linemap);
+	// system("leaks a.out");
 }
